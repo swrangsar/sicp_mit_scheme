ede1747bc8ae0e1e979278da02d1d285b673a245
swrangsar basumatary <swrangsar@example.com>
2001-01-01 05:55:27 +0530

    newton-transform return lambda

--- generalmethod.scm
+++ generalmethod.scm
@@ -111,3 +111,16 @@
 (define (cube x) (* x x x))
 
 ;;; ((deriv cube) 5)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+#|
+(define (sqrt x)
+  (newtons-method (lambda (y) (- (square y) x))
+		  1.0))
+|#
