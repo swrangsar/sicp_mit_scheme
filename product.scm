d9ad87de248f3b359fbb2ab41e32e27e807fa1ef
swrangsar basumatary <swrangsar@example.com>
2001-01-01 05:43:19 +0530

    pi approx with product recursive process

---
+++ product.scm
@@ -0,0 +1,20 @@
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a) (product term (next a) next b))))
+
+(define (factorial n)
+  (define (inc n) (+ n 1))
+  (define (identity x) x)
+  (product identity 1 inc n))
+
+(define (pi-approx b)
+  (define (pi-next x) (+ x 2))
+  (define (pi-ratio b)
+    (* 2
+       b
+       (/ (product square 2 pi-next (- b 2))
+          (product square 3 pi-next (- b 1)))))
+  (if (even? b)
+      (pi-ratio b)
+      (pi-ratio (- b 1))))
