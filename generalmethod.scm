525fb6099011c158f959a4b9c1e6790c3b4d315e
swrangsar basumatary <swrangsar@example.com>
2001-01-01 06:10:20 +0530

    fixed-point-of-transform

--- generalmethod.scm
+++ generalmethod.scm
@@ -124,3 +124,17 @@
   (newtons-method (lambda (y) (- (square y) x))
 		  1.0))
 |#
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+#|
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+			    average-damp
+			    1.0))
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+			    newton-transform
+			    1.0))
+|#
