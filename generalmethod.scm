d229cf97319bd248fb1965bbbfb0234b785f29fb
swrangsar basumatary <swrangsar@example.com>
2016-07-22 20:31:04 +0530

    fixed-point method

--- generalmethod.scm
+++ generalmethod.scm
@@ -26,3 +26,14 @@
 	   (search f b a))
 	  (else
 	   (error "values are not of opposite sign" a b)))))
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
