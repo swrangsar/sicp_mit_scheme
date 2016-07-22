94f4b1ddfae77e65541a93c2922402ac4e77b8cf
swrangsar basumatary <swrangsar@example.com>
2016-07-22 17:35:27 +0530

    start using lambda

--- accumulate.scm
+++ accumulate.scm
@@ -36,3 +36,16 @@
   (define (coprime? a)
     (= (gcd n a) 1))
   (filtered-accumulate coprime? * 1 identity 1 inc n))
+
+(define (pi-sum a b)
+  (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
+       a
+       (lambda (x) (+ x 4))
+       b))
+
+(define (integral f a b dx)
+  (* (sum f
+	  (+ a (/ dx 2.0))
+	  (lambda (x) (+ x dx))
+	  b)
+     dx))
