41c58ceeb0076a49c147a9da245730c0b6c5fa57
swrangsar basumatary <swrangsar@example.com>
2016-07-22 19:09:10 +0530

    general method as procedure

---
+++ generalmethod.scm
@@ -0,0 +1,28 @@
+;;;; general methods
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+	midpoint
+	(let ((test-value (f midpoint)))
+	  (cond ((> test-value 0)
+		 (search f neg-point midpoint))
+		((< test-value 0)
+		 (search f midpoint pos-point))
+		(else midpoint))))))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+	(b-value (f b)))
+    (cond ((and (< a-value 0) (> b-value 0))
+	   (search f a b))
+	  ((and (< b-value 0) (> a-value 0))
+	   (search f b a))
+	  (else
+	   (error "values are not of opposite sign" a b)))))
