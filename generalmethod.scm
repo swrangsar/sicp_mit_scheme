da4884cb5c2320109f0898230949a8d42495ab0c
swrangsar basumatary <swrangsar@example.com>
2016-07-23 21:35:15 +0530

    double of double

--- generalmethod.scm
+++ generalmethod.scm
@@ -143,4 +143,13 @@
   (lambda (x)
     (+ (* (+ (* (+ x a) x) b) x) c)))
 
-(newtons-method (cubic 2 3 5) 1)
+;;; (newtons-method (cubic 2 3 5) 1)
+
+(define (double f)
+  (lambda (x)
+    (f (f x))))
+
+(define (inc x) (+ x 1))
+
+;;; ((double inc) 1)
+;;; (((double (double double)) inc) 5)
