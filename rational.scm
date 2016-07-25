20b18b65ba8330ea239a974786fe9a8aaa81bfab
swrangsar basumatary <swrangsar@example.com>
2016-07-25 12:08:14 +0530

    make segment

--- rational.scm
+++ rational.scm
@@ -51,3 +51,42 @@
 ;;; (print-rat (add-rat one-third one-third))
 ;;; (print-rat (make-rat -1 2))
 ;;; (print-rat (make-rat 1 -5))
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment seg)
+  (car seg))
+
+(define (end-segment seg)
+  (cdr seg))
+
+(define (make-point x y) (cons x y))
+
+(define (x-point p) (car p))
+
+(define (y-point p) (cdr p))
+
+(define (midpoint-segment seg)
+  (let ((start (start-segment seg))
+	(end (end-segment seg)))
+    (make-point
+      (average (x-point start) (y-point start))
+      (average (x-point end) (y-point end)))))
+
+(define (average x y)
+  (/ (+ x y) 2.0))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+;;; (print-point
+;;;   (midpoint-segment
+;;;     (make-segment
+;;;       (make-point 0 1)
+;;;       (make-point 1 0))))
