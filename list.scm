49b680de8a937b0c29798db78e050634869af1c1
swrangsar basumatary <swrangsar@example.com>
2016-07-26 14:02:09 +0530

    count-leaves

--- list.scm
+++ list.scm
@@ -143,5 +143,19 @@
       true
       (helper)))
 
-(for-each (lambda (x) (newline) (display x))
-	  (list 57 321 88))
+;;; (for-each (lambda (x) (newline) (display x))
+;;; 	  (list 57 321 88))
+
+(define (count-leaves x)
+  (cond ((null? x ) 0)
+	((not (pair? x)) 1)
+	(else (+ (count-leaves (car x))
+		 (count-leaves (cdr x))))))
+
+;;; (define x (cons (list 1 2) (list 3 4)))
+;;; 
+;;; (length x)
+;;; (count-leaves x)
+;;; (list x x)
+;;; (length (list x x))
+;;; (count-leaves (list x x))
