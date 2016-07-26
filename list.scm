e81c5be705147593f444cccb72ab4956dc9863f5
swrangsar basumatary <swrangsar@example.com>
2016-07-26 14:32:40 +0530

    crazy car cdr combination

--- list.scm
+++ list.scm
@@ -159,3 +159,7 @@
 ;;; (list x x)
 ;;; (length (list x x))
 ;;; (count-leaves (list x x))
+
+(car (cdaddr (list 1 3 (list 5 7) 9)))
+(caar (list (list 7)))
+(cadadr (cadadr (cadadr (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))))
