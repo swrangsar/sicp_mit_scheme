6403b550a15e79b016efbb44015ed839101605c8
swrangsar basumatary <swrangsar@example.com>
2016-07-26 23:13:53 +0530

    enumerate tree

--- sequence.scm
+++ sequence.scm
@@ -48,3 +48,28 @@
 ;;; 	(else (filter predicate (cdr sequence)))))
 ;;; 
 ;;; (filter odd? (list 1 2 3 4 5))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+
+;;; (accumulate + 0 (list 1 2 3 4 5))
+;;; (accumulate * 1 (list 1 2 3 4 5))
+;;; (accumulate cons nil (list 1 2 3 4 5))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+;;; (enumerate-interval 2 7)
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+	((not (pair? tree)) (list tree))
+	(else (append (enumerate-tree (car tree))
+		      (enumerate-tree (cdr tree))))))
+
+;;; (enumerate-tree sample-tree)
