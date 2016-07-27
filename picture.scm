90d73b8f557619c18b51a2eeaa3676790d56197a
swrangsar basumatary <swrangsar@example.com>
2016-07-27 16:55:15 +0530

    painter func

--- picture.scm
+++ picture.scm
@@ -79,3 +79,12 @@
 
 (define (edge2-frame frame)
   (cddr frame))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+ 	((frame-coord-map frame) (start-segment segment))
+	((frame-coord-map frame) (end-segment segment))))
+     segment-list)))
