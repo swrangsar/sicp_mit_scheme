3d07582b797f25428e0811da96cab6724eb53a2b
swrangsar basumatary <swrangsar@example.com>
2016-12-24 17:47:01 +0530

    reverse encode huffman tree success

--- src/symbolic_data/huffman_tree.scm
+++ src/symbolic_data/huffman_tree.scm
@@ -65,4 +65,31 @@
 (define symbol-frequency-pairs '((A 4) (B 2) (C 1) (D 1)))
 (make-leaf-set symbol-frequency-pairs)
 
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
 
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+
+(define (encode-symbol symbol tree)
+  (let ((symlist (symbols tree)))
+    (if (memq symbol symlist)
+        (cond ((leaf? tree) '())
+              ((memq symbol (symbols (left-branch tree)))
+               (cons 0 (encode-symbol symbol (left-branch tree))))
+              ((memq symbol (symbols (right-branch tree)))
+               (cons 1 (encode-symbol symbol (right-branch tree)))))
+        (error "not found in code tree" symbol))))
+
+(encode (decode sample-message sample-tree) sample-tree)
