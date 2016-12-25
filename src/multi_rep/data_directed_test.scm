71e2b03523c6c7b59c5e992613bd9185d6c312e2
swrangsar basumatary <swrangsar@example.com>
2016-12-25 18:25:59 +0530

    success rational arithmetic package

---
+++ src/multi_rep/data_directed_test.scm
@@ -0,0 +1,53 @@
+(load "symbolic_data/symbolic_data")
+(load "multi_rep/data_directed")
+
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp)) (operands exp)
+                                           var))))
+
+
+(define (same-variable? v1 v2)
+  (and (symbol? v1) (symbol? v2) (eq? v1 v2)))
+
+
+(define (deriv-sum exp var)
+  (make-sum (deriv (addend exp) var)
+            (deriv (augend exp) var)))
+
+(define (deriv-product exp var)
+  (make-sum
+    (make-product (multiplier exp)
+                  (deriv (multiplicand exp) var))
+    (make-product (deriv (multiplier exp) var)
+                  (multiplicand exp))))
+
+(define (install-deriv)
+  (put 'deriv '+ deriv-sum)
+  (put 'deriv '* deriv-product)
+  'done)
+
+(define (deriv-exponentiation expr var)
+  (let ((base (base expr))
+        (exponent (exponent expr)))
+    (make-product exponent
+                  (make-product (make-exponentiation base
+                                                     (make-sum exponent -1))
+                                (deriv base var)))))
+
+(define (install-deriv-exponentiation)
+  (put 'deriv '** deriv-exponentiation)
+  'done)
+
+(install-deriv)
+(install-deriv-exponentiation)
+
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+;;; (put '+ 'deriv deriv-sum)
+;;; (put '* 'deriv deriv-product)
+;;; (put '** 'deriv deriv-exponentiation)
