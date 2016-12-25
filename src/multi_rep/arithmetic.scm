6a62c3fd2f2513cc29ccd29f4623a3240abf8f4d
swrangsar basumatary <swrangsar@example.com>
2016-12-25 18:03:44 +0530

    rename files and folders

---
+++ src/multi_rep/arithmetic.scm
@@ -0,0 +1,7 @@
+(load "multiple_representation/table_2d")
+(load "multiple_representation/data_directed")
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
