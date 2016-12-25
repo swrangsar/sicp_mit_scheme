bf6a9969cbe94117f75841e23c23e1a74ec4e8ab
swrangsar basumatary <swrangsar@example.com>
2016-12-25 21:30:13 +0530

    add data_directed_test again

--- src/multi_rep/data_directed_test.scm
+++ src/multi_rep/data_directed_test.scm
@@ -1,14 +1,15 @@
 (load "symbolic_data/symbolic_data")
 (load "multi_rep/data_directed")
 
-(define (operator exp) (car exp))
-(define (operands exp) (cdr exp))
-
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp) (if (same-variable? exp var) 1 0))
         (else ((get 'deriv (operator exp)) (operands exp)
                                            var))))
+
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
 
 
 (define (same-variable? v1 v2)
