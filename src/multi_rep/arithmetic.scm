f2a9c118e821b5fc2c703c7b3fa1bd5b4fd3a5e8
swrangsar basumatary <swrangsar@example.com>
2016-12-26 10:21:44 +0530

    separate arithmetic test from header

--- src/multi_rep/arithmetic.scm
+++ src/multi_rep/arithmetic.scm
@@ -144,25 +144,3 @@
   (cond ((pair? datum) (cdr datum))
         ((number? datum) datum)
         (error "Bad tagged datum -- CONTENTS" datum)))
-
-(install-scheme-number-package)
-(add (make-scheme-number 3) (make-scheme-number 7))
-
-(install-rational-package)
-(div (make-rational 3 7) (make-rational 21 9))
-
-(install-rectangular-package)
-(install-polar-package)
-(install-complex-package)
-
-(mul (make-complex-from-mag-ang 3 1.57) (make-complex-from-mag-ang 5 1.57))
-(magnitude (make-complex-from-real-imag 3 4))
-(equ? (make-complex-from-mag-ang 3 1.57)
-      (make-complex-from-mag-ang 3 1.57))
-(=zero? (make-scheme-number 0))
-(=zero? (make-scheme-number 0.0))
-(=zero? (make-rational 0 11))
-(=zero? (make-rational 11 0))
-(=zero? (make-complex-from-real-imag 0 0.1))
-(=zero? (make-complex-from-real-imag 0 0))
-(=zero? (make-complex-from-mag-ang 0 10))
---
+++ src/multi_rep/arithmetic_test.scm
@@ -0,0 +1,25 @@
+(load "multi_rep/table_2d")
+(load "multi_rep/data_directed")
+(load "multi_rep/arithmetic")
+
+(install-scheme-number-package)
+(add (make-scheme-number 3) (make-scheme-number 7))
+
+(install-rational-package)
+(div (make-rational 3 7) (make-rational 21 9))
+
+(install-rectangular-package)
+(install-polar-package)
+(install-complex-package)
+
+(mul (make-complex-from-mag-ang 3 1.57) (make-complex-from-mag-ang 5 1.57))
+(magnitude (make-complex-from-real-imag 3 4))
+(equ? (make-complex-from-mag-ang 3 1.57)
+      (make-complex-from-mag-ang 3 1.57))
+(=zero? (make-scheme-number 0))
+(=zero? (make-scheme-number 0.0))
+(=zero? (make-rational 0 11))
+(=zero? (make-rational 11 0))
+(=zero? (make-complex-from-real-imag 0 0.1))
+(=zero? (make-complex-from-real-imag 0 0))
+(=zero? (make-complex-from-mag-ang 0 10))
